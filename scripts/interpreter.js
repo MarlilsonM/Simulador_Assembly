04251b82b10b2f9249e130f2ed9fbcfd062d5673
MarlilsonM <antonio.marlilson.moreira07@aluno.ifce.edu.br>
2024-08-31 10:46:57 -0300

    Expansão do Interpretador
    Implementação de Operações Aritméticas e Lógicas: ADD, SUB, MUL, DIV, AND, OR, e NOT.

--- scripts/interpreter.js
+++ scripts/interpreter.js
@@ -1,7 +1,12 @@
 class Interpreter {
     constructor() {
         this.memory = [];
-        this.registers = {};
+        this.registers = {
+            A: 0, // Registrador A
+            B: 0, // Registrador B
+            C: 0, // Registrador C
+            D: 0  // Registrador D
+        };
         this.currentInstruction = 0;
         this.running = false;
     }
@@ -18,10 +23,80 @@ class Interpreter {
             return;
         }
 
-        const instruction = this.memory[this.currentInstruction].trim();
+        const line = this.memory[this.currentInstruction].trim();
+        const [instruction, ...args] = line.split(' ');
+
+        switch (instruction.toUpperCase()) {
+            case 'ADD':
+                this.add(args);
+                break;
+            case 'SUB':
+                this.sub(args);
+                break;
+            case 'MUL':
+                this.mul(args);
+                break;
+            case 'DIV':
+                this.div(args);
+                break;
+            case 'AND':
+                this.and(args);
+                break;
+            case 'OR':
+                this.or(args);
+                break;
+            case 'NOT':
+                this.not(args);
+                break;
+            // Adicione outras intruções conforme necessário.
+            default:
+                console.error(`Instrução desconhecida: ${instruction}`);
+        }
         // Implementar lógica de execução da instrução
         console.log('Executing:', instruction);
         this.currentInstruction++;
+    }
+
+    add(args) {
+        const [dest, src] = args;
+        this.registers[dest] += this.registers[src];
+        console.log(`ADD ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    sub(args) {
+        const [dest, src] = args;
+        this.registers[dest] -= this.registers[src];
+        console.log(`SUB ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    mul(args) {
+        const [dest, src] = args;
+        this.registers[dest] *= this.registers[src];
+        console.log(`MUL ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    div(args) {
+        const [dest, src] = args;
+        this.registers[dest] /= this.registers[src];
+        console.log(`DIV ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    and(args) {
+        const [dest, src] = args;
+        this.registers[dest] &= this.registers[src];
+        console.log(`AND ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    or(args) {
+        const [dest, src] = args;
+        this.registers[dest] |= this.registers[src];
+        console.log(`OR ${dest}, ${src}: ${this.registers[dest]}`);
+    }
+
+    not(args) {
+        const [dest] = args;
+        this.registers[dest] = ~this.registers[dest];
+        console.log(`NOT ${dest}: ${this.registers[dest]}`);
     }
 
     run(speed) {
@@ -42,7 +117,12 @@ class Interpreter {
 
     reset() {
         this.memory = [];
-        this.registers = {};
+        this.registers = {
+            A: 0,
+            B: 0,
+            C: 0,
+            D: 0
+        };
         this.currentInstruction = 0;
         this.running = false;
     }
