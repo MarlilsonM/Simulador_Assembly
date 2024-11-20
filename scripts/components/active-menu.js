26fdf971c1064be76d8e74ecedd8899bf23aada0
MarlilsonM <antonio.marlilson.moreira07@aluno.ifce.edu.br>
2024-11-20 14:24:31 -0300

    Melhorias na responsividade
    -  Adicionado scroll na tabela de documento
    - Botão do Sidebar foi atualizado
    - Padronização nas medidas de px para rem no media queries do simulador.

--- scripts/components/active-menu.js
+++ scripts/components/active-menu.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
+    const menuToggle = document.getElementById('docs-menu-toggle');
     const sidebar = document.getElementById('sidebar');
-    
+    const chevronIcon = document.getElementById('docs-chevron-icon');
+
     menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('hidden'); // Alterna a classe 'hidden'
+        sidebar.classList.toggle('visible'); // Alterna a classe 'visible'
+        
+        // Alterna o src do chevron
+        if (sidebar.classList.contains('visible')) {
+            chevronIcon.src = '../assets/chevron-left.svg'; // Seta para a esquerda
+        } else {
+            chevronIcon.src = '../assets/chevron-right.svg'; // Seta para a direita
+        }
+    });
+
+    // Fecha o sidebar ao clicar fora dele
+    document.addEventListener('click', (event) => {
+        const isClickInsideSidebar = sidebar.contains(event.target);
+        const isClickOnToggle = menuToggle.contains(event.target);
+
+        // Se o sidebar estiver aberto e o clique não for dentro do sidebar nem no botão de toggle
+        if (sidebar.classList.contains('visible') && !isClickInsideSidebar && !isClickOnToggle) {
+            sidebar.classList.remove('visible'); // Fecha o sidebar
+            chevronIcon.src = '../assets/chevron-right.svg'; // Muda o ícone para a seta para a direita
+        }
     });
 });
